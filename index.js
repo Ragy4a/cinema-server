d311342f5f0a81fc50feb7462b968c403a2259b4
“Ragy4a” <ragy4a@gmail.com>
2024-06-23 09:53:05 +0300

    Installed express and nodemon

--- index.js
+++ index.js
@@ -70,5 +70,58 @@ const pathToAudioFile = './src/test/holy_diver.mp3';
 // console.log(os.arch())
 
 // Process
-process.env.PORT = 5000;
-console.log(process.env)
+// process.env.PORT = 5000;
+// console.log(process.env)
+
+// Create Server with http module
+const HOST_NAME = '127.0.0.1';
+const PORT = 5000;
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+
+
+    console.log(`URL is ${url}.`);
+    console.log(`METHOD is ${req.method}.`);
+
+    res.setHeader('Content-Type', 'text/html', 'charset=utf-8');
+    // res.write('Sigma');
+    // res.end();
+    switch(url) {
+        case '/':
+            console.log('Home page');
+            const homePage = fs.readFileSync('./public/index.html', 'utf8');
+            res.write(homePage);
+            res.end();
+            break;
+        case '/contact':
+            console.log('Contact');
+            const contactPage = fs.readFileSync('./public/contact.html', 'utf8');
+            res.write(contactPage);
+            res.end();
+            break;
+        default: 
+            if (url.includes('/img')) {
+                console.log('Images!')
+                fs.readFile(`./public${url}`, (err, data) => {
+                    if (err) {
+                        res.statusCode = 404;
+                        throw err;
+                    }
+                    res.setHeader('Content-Type', 'image/jpeg')
+                    res.write(data);
+                    res.end();
+                })
+            } else {
+                res.write('<h1>Page not found</h1>');
+                res.end();
+            }
+            // console.log('Default!')
+            // res.write('<h1>Page not found</h1>');
+            // res.end();
+    }
+})
+
+server.listen(PORT, HOST_NAME, () => {
+    console.log(`Сервер запущен и доступен по адрессу http://${HOST_NAME}:${PORT}`)
+});
